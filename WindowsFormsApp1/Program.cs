2cc0a03b758b2d7d38f3703b11c3d9b46fc70329
정인혜 <inhyea36@naver.com>
2018-11-06 17:47:55 +0900

    정인혜 프로젝트

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -17,7 +17,6 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
-            //테스트 중...
         }
     }
 }
